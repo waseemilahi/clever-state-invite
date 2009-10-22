19d40cbcc4d01a09c49d2cff6ec6da2950907223
waseemilahi <waseemilahi@3989e006-b9b2-11de-beb5-195313721cbb>
2009-10-22 17:48:41 +0000

    refactored the files

---
+++ Get Variable Names/Tests/test.h
@@ -0,0 +1,13 @@
+#ifndef TEST_H
+#define TEST_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+# define Global_Var 24
+
+extern void first_test(int a, int b, int c);
+extern int second_test(int a, int b, int c);
+
+#endif
