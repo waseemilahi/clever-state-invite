91489a55a5bd92640a6f39f36f84b23c15561b9e
waseemilahi <waseemilahi@3989e006-b9b2-11de-beb5-195313721cbb>
2009-10-28 18:09:37 +0000

    still refactoring

--- Get Variable Names/get_var_help.c
+++ Get Variable Names/get_var_help.c
@@ -27,3 +27,109 @@ int findsubstr( char *str, char *substr)
   return 0;
 }
 
+/*Get the input and put it into an array of strings.*/
+int get_input(char (*statements)[MAX_LENGTH], char *filename)
+{
+	char ch;
+	int i;
+	char line[MAX_LENGTH];
+	int statement_number = 0;
+	FILE *input;
+
+ for(i = 0; i < MAX_LENGTH; i++)
+    line[i] = '\0';
+  
+  if((input = fopen( filename, "r" )) == NULL)
+    {
+      fprintf(stderr, "\n File Could not be opened for reading. \n\n");
+      exit(1);
+    }
+
+	ch = getc( input );
+
+	while( ch != EOF ) {
+
+	    if(ch == '\n'){
+	      ch = getc(input);
+	      continue;
+	    }
+
+	    if(ch == '#'){
+	      while(1){
+		ch = getc( input );
+		if(ch == '\n'){
+		  ch = getc(input);
+		  break;
+		}
+      }
+      continue;
+    }
+
+    if(ch == '/'){
+      ch = getc(input);
+      if( ch  == '/'){
+	while(1){
+	  ch = getc( input );
+	  if(ch == '\n'){
+	    ch = getc(input);
+	    break;
+	  }
+	}
+     	continue;
+      }
+      else if( ch  == '*'){
+	while(1){
+	  ch = getc( input );
+	  if(ch == '*'){
+	    if( (ch = getc(input)) == '/'){
+	      ch = getc(input);
+	      break;
+	    }
+	  }
+	}
+	continue;
+      }
+    }
+    
+    if(ch == '\t'){
+      ch = getc(input);
+      continue;
+    }
+    
+    if(ch == '"'){
+      while(1){
+	ch = getc( input );
+	if(ch == '"'){
+	  ch = getc(input);
+	  break;
+	}
+      }
+      continue;
+    }
+    
+    
+    if( (ch == '}') || (ch == '{') || (ch == ';') ){
+      
+      if(statement_number >= MAX_LENGTH)break;
+      
+      strcpy(statements[statement_number] , line);
+      
+      statement_number ++;
+      
+      for(i = 0; i < MAX_LENGTH; i++)
+	line[i] = '\0';
+      
+      ch = getc( input );
+	       
+      continue;
+    }
+
+    line[strlen(line)] = ch;
+    
+    ch = getc( input );
+  }
+
+fclose(input);
+return statement_number;
+}
+
