819ceedaa3dc5faf09cf0a7ac42691dc470a5efe
waseemilahi <waseemilahi@3989e006-b9b2-11de-beb5-195313721cbb>
2009-11-19 04:04:41 +0000

    The Pointer and Array variables complete.

--- trunk/Get Variable Names/get_var_help.c
+++ trunk/Get Variable Names/get_var_help.c
@@ -360,7 +360,6 @@ int set_global_constants(char (*statements)[MAX_LENGTH],char (*global_constants)
 	
 	return total_constants;
 	
-
 }
 
 int set_parameters(char *definition, Parameter parameters[])
@@ -368,15 +367,18 @@ int set_parameters(char *definition, Parameter parameters[])
 
 	int i;
 	char *running;
+	char *each_running;
   	char *token;
   	char *tmpt;	
 	int tmpyy = 0;
 	int params = 0;
 	char tmp_dec[MAX_LENGTH];
+	char tmp_dec2[MAX_LENGTH];
 	
 	for(i = 0; i < MAX_LENGTH; i++)
 	{
 		tmp_dec[i] = '\0';
+		tmp_dec2[i] = '\0';
 	}
 	
 	running = strdup(definition);
@@ -426,7 +428,7 @@ int set_parameters(char *definition, Parameter parameters[])
 			tmp_dec[i] = '\0';
 		}
 		
-		token = strtok(running," *[");
+		token = strtok(running," ");
 	
 		tmpt = token;
 			
@@ -447,24 +449,22 @@ int set_parameters(char *definition, Parameter parameters[])
 			tmp_dec[i] = '\0';
 		}
 		
-		if(strcmp(parameters[params].type,"struct") == 0)
+		if( (strcmp(parameters[params].type,"struct") == 0) || (strcmp(parameters[params].type,"unsigned") == 0))
 		{
 		
-			token = strtok(NULL , " *[");
+			token = strtok(NULL , " ");
 			
 					if(token != NULL){
-						int tmpy = 0;
+						//int tmpy = 0;
 						tmpt = token;
-						parameters[params].type[tmpyy] = ' ';
+						parameters[params].type[tmpyy++] = ' ';
 						while(*tmpt != '\0'){
-							tmp_dec[tmpy]=*tmpt;
-							tmpy++;
+							parameters[params].type[tmpyy]=*tmpt;
+							tmpyy++;
 							tmpt++;
 						}
 				
 					}
-					
-			strcat(parameters[params].type,tmp_dec);			
 		
 		} 
 		
@@ -473,7 +473,110 @@ int set_parameters(char *definition, Parameter parameters[])
 			tmp_dec[i] = '\0';
 		}
 		
-		token = strtok(NULL," *[");
+		token = strtok(NULL," []");
+			
+		if(token != NULL){
+		
+			tmpt = token;
+		
+			if(findsubstr(token,"**") == 1){
+					parameters[params].type[tmpyy++] = '*';
+					parameters[params].type[tmpyy++] = '*';
+					tmpt++;
+					tmpt++;
+				}
+				else if(findsubstr(token,"*") == 1){
+					parameters[params].type[tmpyy++] = '*';
+					tmpt++;
+				}			
+		
+			int tmpy = 0;
+			while(*tmpt != '\0'){
+				tmp_dec[tmpy]=*tmpt;
+				tmpy++;
+				tmpt++;
+			}		
+	
+		token = strtok(NULL , "[]");				
+			
+		if( token != NULL){
+					
+			tmpt = token;		
+			parameters[params].type[tmpyy++] = '[';
+				
+			while(*tmpt != '\0'){
+				parameters[params].type[tmpyy]=*tmpt;
+				tmpyy++;
+				tmpt++;
+			}
+					
+			parameters[params].type[tmpyy++] = ']';
+			
+		}
+				
+		token = strtok(NULL , "[]");				
+			
+		if( (token != NULL) && (findsubstr(token,";") == 0)){
+					
+			tmpt = token;			
+			parameters[params].type[tmpyy++] = '[';
+				
+			while(*tmpt != '\0'){
+				parameters[params].type[tmpyy]=*tmpt;
+				tmpyy++;
+				tmpt++;
+			}
+			
+			parameters[params].type[tmpyy++] = ']';
+				
+		}		
+		
+		strcpy(parameters[params++].vars,tmp_dec);
+		}
+		
+		tmpyy = 0;
+		
+		for(i = 0; i < MAX_LENGTH; i++)
+		{
+			tmp_dec[i] = '\0';
+		}
+		
+		return params;
+		
+	}
+	else {
+	
+		strcpy(tmp_dec2,tmp_dec);
+	
+		running = strdup(tmp_dec);
+				
+		token = strtok(running,",");
+		
+		while(token != NULL)
+		{
+		tmpt = token;
+		
+		for(i = 0; i < MAX_LENGTH; i++)
+		{
+			tmp_dec[i] = '\0';
+		}		
+		
+		int tmpy = 0;
+		while(*tmpt != '\0'){
+			tmp_dec[tmpy]=*tmpt;
+			tmpy++;
+			tmpt++;
+		}
+		
+		
+		each_running = strdup(tmp_dec);
+		
+		for(i = 0; i < MAX_LENGTH; i++)
+		{
+			tmp_dec[i] = '\0';
+		}
+		
+		token = strtok(each_running," ");
 	
 		tmpt = token;
 			
@@ -484,112 +587,119 @@ int set_parameters(char *definition, Parameter parameters[])
 				tmpy++;
 				tmpt++;
 			}
+			tmpyy = tmpy;
 		}
-		strcpy(parameters[params++].vars,tmp_dec);
 		
-		return params;
-		
-	}
-	else {
-	
-		running = strdup(tmp_dec);
+		strcpy(parameters[params].type,tmp_dec);
 		
 		for(i = 0; i < MAX_LENGTH; i++)
 		{
 			tmp_dec[i] = '\0';
 		}
 		
-		token = strtok(running," *,");
-		
-		while(token != NULL)
+		if( (strcmp(parameters[params].type,"struct") == 0) || (strcmp(parameters[params].type,"unsigned") == 0))
 		{
 		
-			tmpt = token;
-		
-			if(token != NULL){
-				int tmpy = 0;
-				while(*tmpt != '\0'){
-					tmp_dec[tmpy]=*tmpt;
-					tmpy++;
-					tmpt++;
-				}
-				tmpyy = tmpy;
-			}
-			
-			if( findsubstr(tmp_dec, "]") == 1){ 
-				
-				token = strtok(NULL," *,");
-				
-				for(i = 0; i < MAX_LENGTH; i++)
-				{
-					tmp_dec[i] = '\0';
-				}
-				
-				tmpt = token;
-		
-				if(token != NULL){
-					int tmpy = 0;
-					while(*tmpt != '\0'){
-						tmp_dec[tmpy]=*tmpt;
-						tmpy++;
-						tmpt++;
-					}
-					tmpyy = tmpy;
-				}				
-				
-			}			
-		
-			strcpy(parameters[params].type,tmp_dec);
-		
-			for(i = 0; i < MAX_LENGTH; i++)
-			{
-				tmp_dec[i] = '\0';
-			}
-		
-			if(strcmp(parameters[params].type,"struct") == 0)
-			{
-		
-				token = strtok(NULL , " *");
+			token = strtok(NULL , " ");
 			
 					if(token != NULL){
-						int tmpy = 0;
+						//int tmpy = 0;
 						tmpt = token;
-						parameters[params].type[tmpyy] = ' ';
+						parameters[params].type[tmpyy++] = ' ';
 						while(*tmpt != '\0'){
-							tmp_dec[tmpy]=*tmpt;
-							tmpy++;
+							parameters[params].type[tmpyy]=*tmpt;
+							tmpyy++;
 							tmpt++;
 						}
 				
 					}
-					
-				strcat(parameters[params].type,tmp_dec);			
 		
-			} 
+		} 
 		
-			for(i = 0; i < MAX_LENGTH; i++)
-			{
-				tmp_dec[i] = '\0';
-			}
+		for(i = 0; i < MAX_LENGTH; i++)
+		{
+			tmp_dec[i] = '\0';
+		}
 		
-			token = strtok(NULL," *[,");
-	
-			tmpt = token;
+		token = strtok(NULL," []");
 			
-			if(token != NULL){
-				int tmpy = 0;
-				while(*tmpt != '\0'){
-					tmp_dec[tmpy]=*tmpt;
-					tmpy++;
+		if(token != NULL){
+		
+			tmpt = token;
+		
+			if(findsubstr(token,"**") == 1){
+					parameters[params].type[tmpyy++] = '*';
+					parameters[params].type[tmpyy++] = '*';
+					tmpt++;
 					tmpt++;
 				}
-			}
+				else if(findsubstr(token,"*") == 1){
+					parameters[params].type[tmpyy++] = '*';
+					tmpt++;
+				}			
 		
-			strcpy(parameters[params++].vars,tmp_dec);
+			int tmpy = 0;
+			while(*tmpt != '\0'){
+				tmp_dec[tmpy]=*tmpt;
+				tmpy++;
+				tmpt++;
+			}		
+	
+		token = strtok(NULL , "[]");				
 			
-			token = strtok(NULL, " ,");
+		if( token != NULL){
+					
+			tmpt = token;		
+			parameters[params].type[tmpyy++] = '[';
+				
+			while(*tmpt != '\0'){
+				parameters[params].type[tmpyy]=*tmpt;
+				tmpyy++;
+				tmpt++;
+			}
+					
+			parameters[params].type[tmpyy++] = ']';
+			
+		}
+				
+		token = strtok(NULL , "[]");				
+			
+		if( token != NULL){
+					
+			tmpt = token;			
+			parameters[params].type[tmpyy++] = '[';
+				
+			while(*tmpt != '\0'){
+				parameters[params].type[tmpyy]=*tmpt;
+				tmpyy++;
+				tmpt++;
+			}
+			
+			parameters[params].type[tmpyy++] = ']';
+				
+		}		
+		
+		strcpy(parameters[params].vars,tmp_dec);
+		
+		params++;
+		}		
+		
+			running = strdup(tmp_dec2);
+			token = strtok(running, ",");
+			for(i = 0; i < params;i++){
+				if(token != NULL)token = strtok(NULL,",");
+				else break;
+			}
 			
 		}		
+		
+		tmpyy = 0;
+		
+		for(i = 0; i < MAX_LENGTH; i++)
+		{
+			tmp_dec[i] = '\0';
+			tmp_dec2[i] = '\0';
+		}
 		
 		return params;
 			
