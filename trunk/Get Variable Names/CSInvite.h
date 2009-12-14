0888eb98c5ab213752f1dbc504eedd309951f46a
waseemilahi <waseemilahi@3989e006-b9b2-11de-beb5-195313721cbb>
2009-12-14 19:36:16 +0000

    formatting.

--- trunk/Get Variable Names/CSInvite.h
+++ trunk/Get Variable Names/CSInvite.h
@@ -1,5 +1,5 @@
-#ifndef GET_VAR_H
-#define GET_VAR_H
+#ifndef CSINVITE_H
+#define CSINVITE_H
 
 /* 
    Include all the required header files 
@@ -69,7 +69,7 @@ typedef struct scoped_variables{
 }LocalVar;
 
 /*
-	These functions get called once for each time the program executes
+	These functions get called once for each time the program executes (defined in CSInvite_set_input.c)
 */
 extern int get_input(char (*statements)[MAX_LENGTH], char *filename);
 extern int get_possible_constants(char (*statements)[MAX_LENGTH], int statement_number);
@@ -78,19 +78,22 @@ extern int set_global_variables(char (*statements)[MAX_LENGTH],GlobalVar global_
 extern int set_functions(char (*statements)[MAX_LENGTH],Functions function_list[],int statement_number);
 
 /*
-	These Functions are local to each function
+	These Functions are local to each function (defined in CSInvite_set_func.c)
 */
 extern int find_function(int function_number,char *funcs, Functions function_list[]);
 extern int set_parameters(char *definition, Parameter parameters[]);
 extern int set_statement_scopes(char * definition, Scoped_Statements function_scoped_statements[]);
 extern int set_declared_local_variables(Scoped_Statements function_scoped_statements[], int total_scoped_statements, LocalVar declared_local_variables[],Parameter parameters[],int total_params, GlobalVar global_variables[], int total_globals, char (*global_constants)[MAX_LENGTH], int total_constants);
 
+/*
+	These functions are called for each function that needs to be checked (recursive calls + original) (defined in CSInvite_set_vars.c)
+*/
 extern int set_dependency(int total_dependent_variables,char *statement,int scope,int number, int total_local_variables, LocalVar local_variables[],char (*dependent_variables)[28],Functions function_list[],Parameter parameters[],GlobalVar global_variables[],char (*global_constants)[MAX_LENGTH]);
 extern int set_variables(char (*done_func)[VAR_LENGTH], int total_done,int total_variables,int function_number, Functions function_list[],int total_params,Parameter parameters[],int total_globals,GlobalVar global_variables[],int total_constants,char (*global_constants)[MAX_LENGTH],int total_dependent_variables,char (*dependent_variables)[28],Variable variables[]);
 extern int get_func_vars(char (*done_func)[VAR_LENGTH], int total_done,char *func,int function_number,int total_globals,int total_constants,char *definition,int total_dependent_variables,char (*dependent_variables)[28],int total_variables,Variable variables[],Functions function_list[],GlobalVar global_variables[],char (*global_constants)[MAX_LENGTH]);
 
 /*
-	String Helper Functions (defined in get_var_help.c)
+	String Helper Functions (defined in CSInvite_help.c)
 */
 extern char *trimwhitespace(char *str);
 extern int findsubstr(char *string , char *substring);
